eefff0602a7b2e63abc91387f200ef1a1fd4e8e4
rebeca.farias <becacamargodefarias@gmail.com>
2022-09-19 17:32:02 -0300

    projeto dado, finalizado.

--- app/src/main/java/com/example/dadorun/MainActivity.kt
+++ app/src/main/java/com/example/dadorun/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dadorun
 
 import android.app.ProgressDialog.show
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -22,8 +24,17 @@ class MainActivity : AppCompatActivity() {
     private fun jogarDado() {
         val dado = Dado(6)
         val jogarDado = dado.jogar()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = jogarDado.toString()
+        val imagemDado: ImageView = findViewById(R.id.imageView)
+
+        when (jogarDado) {
+            1 -> imagemDado.setImageResource(R.drawable.dado_1)
+            2 -> imagemDado.setImageResource(R.drawable.dado_2)
+            3 -> imagemDado.setImageResource(R.drawable.dado_3)
+            4 -> imagemDado.setImageResource(R.drawable.dado_4)
+            5 -> imagemDado.setImageResource(R.drawable.dado_5)
+            6 -> imagemDado.setImageResource(R.drawable.dado_6)
+        }
+
     }
 } class Dado(val numeroLados: Int) {
 
